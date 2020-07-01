3c79c3a8d33c260114b1d051a96c53f5dcddd482
Dalo <dali.molochnik@gmail.com>
2020-07-01 08:29:43 -0300

    API: installing body-parser, postman and using GET and POST

--- server.js
+++ server.js
@@ -1,8 +1,41 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+var ingredients = [
+  {
+    'id':'00001',
+    'name':'Milk'
+  },
+  {
+    'id':'00002',
+    'name':'Egg'
+  },
+  {
+    'id':'00003',
+    'name':'Chocolate'
+  },
+  {
+    'id':'00004',
+    'name':'Chicken'
+  }
+]
+
 app.get('/', function(request, response){
-  response.send('My first API');
+  response.send(ingredients);
+});
+
+app.post('/', function(request, response){
+  var ingredient = request.body;
+  if (!ingredient || ingredient.name === '') {
+    response.status(500).send('Motherfoooo put an ingredient!');
+  } else {
+    ingredients.push(ingredient);
+    response.status(200).send('Your ingredient named ' + ingredient.name + ' has been added succesfully');
+  }
 });
 
 app.listen(3000, function(){
